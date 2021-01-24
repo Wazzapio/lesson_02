608df8e5ac574ed2679112091597887240753232
Toks <myster1on.toks@gmail.com>
2021-01-24 17:51:50 +0300

    add fixes

--- hw_2/task_2.py
+++ hw_2/task_2.py
@@ -1,19 +1,24 @@
 my_list = []
 num = 1
-print("Приветствую, давайте создадим список из 4-ех эл-тов.")
+ind_0 = 0
+ind_1 = 1
 
-while len(my_list) < 4:
+
+print("Приветствую, давайте создадим список из эл-тов.")
+num_max = int(input('Укажите количество эл-тов: '))
+
+while len(my_list) < num_max:
     el = input(f"{num}-й элемент: ")
     my_list.append(el)
     num += 1
 
 print(my_list)
 
-print("\nА теперь поменяем значения элементов с индексами 0 и 1, а так же 2 и 3.")
+print("\nА теперь поменяем значения элементов с индексами 0 и 1, а так же 2 и 3 и т.д.")
 
-my_list[0], my_list[1] = my_list[1], my_list[0]
-my_list[2], my_list[3] = my_list[3], my_list[2]
-
-print(my_list)
+while ind_1 != len(my_list):
+    my_list[ind_0], my_list[ind_1] = my_list[ind_1], my_list[ind_0]
+    ind_0 += 2
+    ind_1 += 2
 
 
